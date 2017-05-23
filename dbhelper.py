a631ab2f60ca548f527429b83fbac366f5f6d588
Tom Stoll <tom@ubnt.com>
2017-05-23 15:53:28 -0500

    dbhelper.py, sql injection fix before and after.

--- dbhelper.py
+++ dbhelper.py
@@ -22,10 +22,12 @@ class DBHelper:
     def add_input(self, data):
         connection = self.connect()
         try:
-            # The following introduces a deliberate security flaw.
-            # SQL Injection
+            # The following introduces a SQL Injection security flaw.
+            #query = "INSERT INTO crimes (description) VALUES ('{}');".format(data)
+            # The following fixes the SQL Injection security flaw.
             query = "INSERT INTO crimes (description) VALUES (%s);"
             with connection.cursor() as cursor:
+                #cursor.execute(query)
                 cursor.execute(query, data)
                 connection.commit()
         finally:
